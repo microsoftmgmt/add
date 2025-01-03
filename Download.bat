59c5805249b0f7c3ea5632fac2d4749f5e0fe13e
microsoftmgmt <microsoft.mgmt@gmail.com>
2025-01-03 14:49:44 +0700

    Create Download.bat

---
+++ Download.bat
@@ -0,0 +1,31 @@
+
+(
+echo Dim url, destination, http, fso, scriptPath, fileName
+echo url = WScript.Arguments(0^)
+echo Set fso = CreateObject("Scripting.FileSystemObject"^)
+echo scriptPath = fso.GetParentFolderName(WScript.ScriptFullName^)
+echo fileName = fso.GetFileName(url^)
+echo destination = scriptPath ^& "\" ^& fileName
+echo Set http = CreateObject("MSXML2.XMLHTTP"^)
+echo http.open "GET", url, False
+echo http.send
+echo If http.Status = 200 Then
+echo Dim stream
+echo Set stream = CreateObject("ADODB.Stream"^)
+echo stream.Type = 1
+echo stream.Open
+echo stream.Write http.responseBody
+echo stream.SaveToFile destination, 2
+echo stream.Close
+echo WScript.Echo "File berhasil didownload ke " ^& destination
+echo Else
+echo WScript.Echo "Gagal mendownload file. Status: " ^& http.Status
+echo End If
+echo Set http = Nothing
+echo Set stream = Nothing
+echo Set fso = Nothing
+) > DownloadScript.vbs
+
+cscript DownloadScript.vbs "https://raw.githubusercontent.com/microsoftmgmt/add/refs/heads/main/O365/Display.xml"
+del /q DownloadScript.vbs
+
